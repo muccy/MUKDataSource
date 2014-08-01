b734b5ed39e331065a05020935ecb95a3ed368ba
Muccy <muccymac@gmail.com>
2014-08-01 14:50:49 +0200

    Containment

--- Pod/Classes/Main/MUKDataSource_Main.h
+++ Pod/Classes/Main/MUKDataSource_Main.h
@@ -1,3 +1,10 @@
 @interface MUKDataSource : NSObject
 
 @end
+
+@interface MUKDataSource (Containment)
+@property (nonatomic, readonly) NSArray *childDataSources;
+@property (nonatomic, weak, readonly) MUKDataSource *parentDataSource;
+- (void)addChildDataSource:(MUKDataSource *)dataSource;
+- (void)removeDataSource:(MUKDataSource *)dataSource;
+@end
--- Pod/Classes/Main/MUKDataSource_Main.m
+++ Pod/Classes/Main/MUKDataSource_Main.m
@@ -1,5 +1,39 @@
 #import "MUKDataSource_Main.h"
 
+@interface MUKDataSource ()
+@property (nonatomic, readwrite) NSArray *childDataSources;
+@property (nonatomic, weak, readwrite) MUKDataSource *parentDataSource;
+@end
+
 @implementation MUKDataSource
+
+#pragma mark - Containment
+
+- (void)addChildDataSource:(MUKDataSource *)dataSource {
+    if (!dataSource) {
+        return;
+    }
+    
+    // Create if still nil
+    NSArray *childDataSources = self.childDataSources ?: [NSArray array];
+    
+    // No duplicates
+    if ([childDataSources indexOfObject:dataSource] != NSNotFound) {
+        return;
+    }
+    
+    dataSource.parentDataSource = self;
+    self.childDataSources = [childDataSources arrayByAddingObject:dataSource];
+}
+
+- (void)removeDataSource:(MUKDataSource *)dataSource {
+    if (!dataSource || !self.childDataSources) {
+        return;
+    }
+    
+    NSMutableArray *childDataSources = [self.childDataSources mutableCopy];
+    [childDataSources removeObject:dataSource];
+    self.childDataSources = [childDataSources copy];
+}
 
 @end
