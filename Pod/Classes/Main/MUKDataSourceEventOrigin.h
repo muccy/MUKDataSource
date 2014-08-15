248b546ba14fe76dfd5e5cabfacd71379182f1e7
Muccy <muccymac@gmail.com>
2014-08-15 16:31:40 +0200

    Event original header documented

--- Pod/Classes/Main/MUKDataSourceEventOrigin.h
+++ Pod/Classes/Main/MUKDataSourceEventOrigin.h
@@ -1,7 +1,19 @@
 #import <Foundation/Foundation.h>
 
+/**
+ The origin of an event inside the data source.
+ */
 typedef NS_ENUM(NSInteger, MUKDataSourceEventOrigin) {
+    /**
+     The origin cannot be estabilished.
+     */
     MUKDataSourceEventOriginUnknown = 0,
+    /**
+     The origin is programmatic.
+     */
     MUKDataSourceEventOriginProgrammatic,
+    /**
+     The origin is user interaction (like gesture on table view cells and so on).
+     */
     MUKDataSourceEventOriginUserInteraction
 };
