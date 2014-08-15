de4c943e85f73fa0f2204ed7745d17fb5bdb3da8
Muccy <muccymac@gmail.com>
2014-08-15 20:37:42 +0200

    Content loading documented

--- Pod/Classes/Content Loading/MUKDataSourceContentLoading.h
+++ Pod/Classes/Content Loading/MUKDataSourceContentLoading.h
@@ -2,12 +2,40 @@
 #import <MUKDataSource/MUKDataSourceContentLoadingResultType.h>
 
 @class MUKDataSource;
+/**
+ An object which encapsulates a single content loading for data source.
+ */
 @interface MUKDataSourceContentLoading : NSObject
+/**
+ The loading state of data source when content loading is created.
+ */
 @property (nonatomic, copy, readonly) NSString *sourceState;
+/**
+ The data source which has requested content loading creation.
+ */
 @property (nonatomic, weak, readonly) MUKDataSource *dataSource;
+/**
+ YES if this content loading is still actual.
+ You should test against this property to know if it's proper to abort early.
+ */
 @property (nonatomic, readonly, getter = isValid) BOOL valid;
+/**
+ A block which contains content loading logic.
+ It is executed by data source.
+ */
 @property (nonatomic, copy) dispatch_block_t job;
-
+/**
+ Finishes content loading.
+ You should call this method inside job block in order to finish and update data
+ source.
+ You can also call this method with MUKDataSourceContentLoadingResultTypeCancelled
+ to cancel a content loading.
+ @param resultType Tells data source how content loading has finished. This is used
+ to know next loadingState.
+ @param error Content loading completion error.
+ @param updateHandler A block executed when state is transitioning to new loading
+ state (before it is set). In this block you should setup new data source contents,
+ like items and childDataSources.
+ */
 - (void)finishWithResultType:(MUKDataSourceContentLoadingResultType)resultType error:(NSError *)error update:(dispatch_block_t)updateHandler;
-
 @end
--- Pod/Classes/Content Loading/MUKDataSourceContentLoadingResultType.h
+++ Pod/Classes/Content Loading/MUKDataSourceContentLoadingResultType.h
@@ -1,9 +1,27 @@
 #import <Foundation/Foundation.h>
 
+/**
+ How content loading has finished.
+ */
 typedef NS_ENUM(NSInteger, MUKDataSourceContentLoadingResultType) {
+    /**
+     Content loading has found all requested data.
+     */
     MUKDataSourceContentLoadingResultTypeComplete,
+    /**
+     Content loading has found a part of requested data.
+     */
     MUKDataSourceContentLoadingResultTypePartial,
+    /**
+     Content loading has found no data.
+     */
     MUKDataSourceContentLoadingResultTypeEmpty,
+    /**
+     Content loading has encountered an error.
+     */
     MUKDataSourceContentLoadingResultTypeError,
+    /**
+     Content loading has been cancelled.
+     */
     MUKDataSourceContentLoadingResultTypeCancelled
 };
