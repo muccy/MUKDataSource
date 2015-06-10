297d2dd93fd6ab145dfdb1d8a90d84ff6cbe4782
Muccy <muccymac@gmail.com>
2015-06-10 13:51:53 +0200

    Tabula rasa

--- Pod/Classes/Private/MUKDataSourceBatch.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@interface MUKDataSourceBatch : NSObject
-@property (nonatomic, readonly) NSArray *blocks;
-- (void)addBlock:(dispatch_block_t)block;
-- (void)performAllBlocks;
-@end
