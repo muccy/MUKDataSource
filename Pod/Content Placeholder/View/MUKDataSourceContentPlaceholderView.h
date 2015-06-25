c6d03ba1003d171012ec148968dd39703fccd156
Muccy <muccymac@gmail.com>
2015-06-25 10:27:13 +0200

    Placeholder view

---
+++ Pod/Content Placeholder/View/MUKDataSourceContentPlaceholderView.h
@@ -0,0 +1,31 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ A view which contains centered title and text labels and an image view on top.
+ It is ideal to be used as user interface to show a fullscreen placeholder.
+ */
+@interface MUKDataSourceContentPlaceholderView : UIView
+/**
+ Title label.
+ */
+@property (nonatomic, weak, readonly) UILabel *titleLabel;
+/**
+ Text label.
+ It is layed out under titleLabel.
+ */
+@property (nonatomic, weak, readonly) UILabel *textLabel;
+/**
+ Image view.
+ It is layed out above titleLabel.
+ */
+@property (nonatomic, weak, readonly) UIImageView *imageView;
+/**
+ Margins of title label.
+ Default: 10 pt to image (top); 5 pt to left/right; 5 pt to text (bottom).
+ */
+- (UIEdgeInsets)titleLabelMinimumMargins;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pod/Content Placeholder/View/MUKDataSourceContentPlaceholderView.m
@@ -0,0 +1,111 @@
+#import "MUKDataSourceContentPlaceholderView.h"
+
+@interface MUKDataSourceContentPlaceholderView ()
+@property (nonatomic, weak, readwrite) UILabel *titleLabel, *textLabel;
+@property (nonatomic, weak, readwrite) UIImageView *imageView;
+@end
+
+@implementation MUKDataSourceContentPlaceholderView
+
+- (id)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        CommonInit(self);
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        CommonInit(self);
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    UIEdgeInsets const titleLabelMinimumMargins = [self titleLabelMinimumMargins];
+    CGFloat const availableWidth = CGRectGetWidth(self.bounds) - titleLabelMinimumMargins.left - titleLabelMinimumMargins.right;
+    [self.titleLabel sizeToFit];
+    CGRect frame = self.titleLabel.frame;
+    if (frame.size.width > availableWidth) {
+        frame.size.width = availableWidth;
+    }
+    self.titleLabel.frame = frame;
+    
+    frame = self.textLabel.frame;
+    frame.size.width = availableWidth;
+    self.textLabel.frame = frame;
+    [self.textLabel sizeToFit];
+    
+    [self.imageView sizeToFit];
+    
+    // Center horizontally
+    frame = self.titleLabel.frame;
+    frame.origin.x = roundf(CGRectGetMidX(self.bounds) - CGRectGetWidth(frame)/2.0f);
+    self.titleLabel.frame = frame;
+    
+    frame = self.textLabel.frame;
+    frame.origin.x = roundf(CGRectGetMidX(self.bounds) - CGRectGetWidth(frame)/2.0f);
+    self.textLabel.frame = frame;
+    
+    frame = self.imageView.frame;
+    frame.origin.x = roundf(CGRectGetMidX(self.bounds) - CGRectGetWidth(frame)/2.0f);
+    self.imageView.frame = frame;
+    
+    // Center vertically as a block
+    CGFloat const imageToTitleMargin = titleLabelMinimumMargins.top;
+    CGFloat const titleToTextMargin = titleLabelMinimumMargins.bottom;
+    CGFloat const totalVerticalHeight = CGRectGetHeight(self.imageView.frame) + imageToTitleMargin + CGRectGetHeight(self.titleLabel.frame) + titleToTextMargin + CGRectGetHeight(self.textLabel.frame);
+    CGFloat const remainingVerticalSpace = CGRectGetHeight(self.bounds) - totalVerticalHeight;
+    
+    frame = self.imageView.frame;
+    frame.origin.y = roundf(remainingVerticalSpace/2.0f);
+    self.imageView.frame = frame;
+    
+    frame = self.titleLabel.frame;
+    frame.origin.y = CGRectGetMaxY(self.imageView.frame) + imageToTitleMargin;
+    self.titleLabel.frame = frame;
+    
+    frame = self.textLabel.frame;
+    frame.origin.y = CGRectGetMaxY(self.titleLabel.frame) + titleToTextMargin;
+    self.textLabel.frame = frame;
+}
+
+- (UIEdgeInsets)titleLabelMinimumMargins {
+    return UIEdgeInsetsMake(10.0f, 5.0f, 5.0f, 5.0f);
+}
+
+#pragma mark - Private
+
+static void CommonInit(MUKDataSourceContentPlaceholderView *me) {
+    [me insertAllSubviews];
+}
+
+- (void)insertAllSubviews {
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+    label.backgroundColor = [UIColor clearColor];
+    label.textColor = [UIColor darkTextColor];
+    label.font = [UIFont preferredFontForTextStyle:UIFontTextStyleHeadline];
+    label.numberOfLines = 1;
+    label.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:label];
+    self.titleLabel = label;
+    
+    label = [[UILabel alloc] initWithFrame:CGRectZero];
+    label.backgroundColor = [UIColor clearColor];
+    label.textColor = [UIColor lightGrayColor];
+    label.font = [UIFont preferredFontForTextStyle:UIFontTextStyleSubheadline];
+    label.numberOfLines = 0;
+    label.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:label];
+    self.textLabel = label;
+    
+    UIImageView *imageView = [[UIImageView alloc] init];
+    [self addSubview:imageView];
+    self.imageView = imageView;
+}
+
+@end
